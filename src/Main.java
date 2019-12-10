484f52873a91f320a77da8c3519a5e60eba67415
Timur <mr.tiger171@mail.ru>
2019-12-10 20:44:00 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ps4 top, pk govno");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+        System.out.println("PUBG");
+    }
+
+}
---
+++ src/bybellhard/j19/lessons/lesson02/PrimitivesExample.java
@@ -0,0 +1,23 @@
+package bybellhard.j19.lessons.lesson02;
+
+// bybellhard.j19.lessons.lesson02.PrimitivesExample
+
+import java.sql.SQLOutput;
+
+public class PrimitivesExample {
+
+
+    //psvm
+    public static void main(String[] args) {
+        short sh1 = 5;
+        int i = 100;
+        double d = 3.14;
+        System.out.println((sh1 - d) / i);
+
+        int o = 0;
+        System.out.println(o++);
+
+
+        System.out.println(++o);
+    }
+}
